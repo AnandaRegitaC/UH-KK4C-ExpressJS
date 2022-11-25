0b28170878774d5906df1e31a3cce077aec780f2
AnandaRegitaC <anandaregitac@gmail.com>
2022-11-25 23:17:18 +0700

    uh kk4c

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/praktikumkk4c');
-  console.log("success connect mongo")
+  console.log("DB Connected")
   
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
--- router/users.js
+++ router/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+
 const usercontroller = require('../controllers/user')
 
 router.get('/users', usercontroller.index)
